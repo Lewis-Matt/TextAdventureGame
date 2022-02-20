222c4daaed687e857d8c46a9e8c2f8e6dd31fde2
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-20 11:36:22 -0600

    initial start condition

--- src/TextGame.java
+++ src/TextGame.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class TextGame {
 
+    public static void game() {
+        System.out.println("Are you ready to begin your adventure? [y/n]");
+        Scanner scanner = new Scanner(System.in);
+        String startResponse = scanner.next();
+        if (startResponse.equalsIgnoreCase("y") || startResponse.equalsIgnoreCase("yes")) {
 
 
+
+
+
+
+
+        } else {
+            System.out.println("Okay, return at your leisure (coward).");
+        }
+    }
+
+//////////////////////////////////////////////////////////MAIN//////////////////////////////////////////////////////////
+    public static void main(String[] args) {
+        game();
+    }
 }
